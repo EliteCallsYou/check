37555852181fe35d274ddd9edb52c83901feb96a
= <shuhratabduqodirov5@gmail.com>
2023-06-07 16:07:19 +0500

    Update data

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import streamlit as st
 import time
 
 def commit_to_git():
-    subprocess.call(['git', 'add', '--all'])
+    subprocess.call(['git', 'add', '.'])
     subprocess.call(['git', 'commit', '-m', 'Update data'])
-    subprocess.call(['git', 'push', 'origin', 'main'])
+    subprocess.call(['git', 'push'])
 
 if __name__ == '__main__' :
     conn = sqlite3.connect('data.db')
@@ -18,12 +18,29 @@ if __name__ == '__main__' :
     # );""") fdsafasdfsadfsdasdfsadfasfsfdsfsadf
 
 
-    cursor.execute("insert into logs values (%s)"%str( round(time.time()) ) )
+    # cursor.execute("insert into logs values (%s)"%str( round(time.time()) ) )
 
-    conn.commit()
+    # conn.commit()
 
 
-    st.write( cursor.execute("select * from logs").fetchall() )
+    # st.write( cursor.execute("select * from logs").fetchall() )
+
+
+    new = pd.DataFrame( data={
+        'time':[time.time()]
+        } )
+
+
+    data = pd.read_csv('data.csv')
+
+
+    data = pd.concat([data,new])
+
+    data.to_csv('data.csv',index=False)
+
+
+    st.write( data )
+
 
     commit_to_git()
     st.write('Changes committed and pushed to GitHub.')
