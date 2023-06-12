97f3e314b900edc824bf10b3588369a606f6afc8
Shuhrat <shuhratabduqodirov5@gmail.com>
2023-06-12 19:03:03 +0500

    fsdafas

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import streamlit as st
 import time
 
 def commit_to_git():
+    subprocess.call(['git' , 'config', '--global user.email', ' shuhratabduqodirov5@gmail.com '])
+    subprocess.call(['git', 'config', '--global', 'user.name', 'Shuhrat' ])
+    
     subprocess.call(['git', 'add', '.'])
     subprocess.call(['git', 'commit', '-m', 'Update data'])
     subprocess.call(['git', 'push'])
