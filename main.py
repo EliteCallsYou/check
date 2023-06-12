c666cab1154ab70967f8874a91e92c641e0842a8
Shuhrat <shuhratabduqodirov5@gmail.com>
2023-06-12 19:07:06 +0500

    fdssdf

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ def commit_to_git():
     subprocess.call(['git' , 'config', '--global', 'user.email', ' shuhratabduqodirov5@gmail.com '])
     subprocess.call(['git', 'config', '--global', 'user.name', 'Shuhrat' ])
 
-    time.sleep(2)
-    subprocess.call(['elitecallsyou'])
-    time.sleep(1)
-    subprocess.call(['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'])
+    # time.sleep(2)
+    # subprocess.call(['elitecallsyou'])
+    # time.sleep(1)
+    # subprocess.call(['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'])
 
-    subprocess.call(['git', 'add', '.'])
-    subprocess.call(['git', 'commit', '-m', 'Update data'])
-    subprocess.call(['git', 'push'])
+    # subprocess.call(['git', 'add', '.'])
+    # subprocess.call(['git', 'commit', '-m', 'Update data'])
+    # subprocess.call(['git', 'push'])
 
 if __name__ == '__main__' :
 #     conn = sqlite3.connect('data.db')
